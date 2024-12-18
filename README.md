6f0809dd5324e4c8bf5d87455f0e1d2c8881e31b
bryskiewiczr <bryskiewiczr@protonmail.com>
2024-12-19 00:22:22 +0100

    gun work in progress

---
+++ README.md
@@ -0,0 +1,2 @@
+https://www.youtube.com/watch?v=GwCiGixlqiU
+@ 1:10:58
--- mob.gd
+++ mob.gd
@@ -1,10 +1,7 @@
 extends CharacterBody2D
 
 
-var player
-
-func _ready():
-	player = get_node("/root/Game/Player")
+@onready var player = get_node("/root/Game/Player")
 	
 func _physics_process(delta):
 	var direction = global_position.direction_to(player.global_position)
